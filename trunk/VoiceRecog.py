2aaeab8972e79621254d01e1748a426352358e17
santamarinafj@gmail.com <santamarinafj@gmail.com@2284b874-346c-ab0b-b1a8-31b75f95b9cf>
2011-12-14 23:34:06 +0000

    updated to work better with anyone's voice since it trains initially from the users voice instead of hard coded magbins

--- trunk/VoiceRecog.py
+++ trunk/VoiceRecog.py
@@ -1,3 +1,10 @@
+#Version 2
+
+#Created by:
+#   Fernando Santamarina
+#   Tyler Cloke
+#   Zach Stiggelbout
+
 import wave
 import struct
 import scipy
@@ -20,18 +27,23 @@ global myCanvas
 global toolbar
 global f
 
+TRAIN_COUNT = 0
+VECTORS_TO_TRAIN = []
+
+#sample dictionary, make sure to have numOutputs = length(dictionary)
+dictionary = ['yes', 'no', 'maybe']
+
 weightsYes = []
 weightsNo = []
 weightsMaybe = []
 currentMagBin = []
 
-learning_rate = 0.1
+learning_rate = 0.07
 inputSize = 20
-numOutputs = 3
+numOutputs = len(dictionary)
 
 
-#sample dictionary, make sure to have numOutputs = length(dictionary)
-dictionary = ['yes', 'no', 'maybe']
+
 
 
 
@@ -50,7 +62,8 @@ def sum_sig(values, word):
       return sum(math.tanh(value*weightsMaybe[index]) for index, value in enumerate(values))
 
 
-
+#Called to train the word that the perceptron
+#should actualy guess
 def trainActual():
    word = entry.get().lower()
    if word not in dictionary:
@@ -72,13 +85,15 @@ def trainActual():
       entry.delete(0, len(word))
       entry.insert(0, word + ' learned')
 
-#trains all the perceptrons by calling their respective train method
+#trains all the perceptrons using the initial data collected
+#by calling their respective train method
 def train(training_set):
-   trainYes([[training_set[0], 1], [training_set[1], 0], [training_set[2], 0]])
-   trainNo([[training_set[0], 0], [training_set[1], 1], [training_set[2], 0]])
-   trainMaybe([[training_set[0], 0], [training_set[1], 0], [training_set[2], 1]])
+   trainYes([[training_set[0], 1], [training_set[1], 1], [training_set[2], 0], [training_set[3], 0], [training_set[4], 0], [training_set[5], 0]])
+   trainNo([[training_set[0], 0], [training_set[1], 0], [training_set[2], 1], [training_set[3], 1], [training_set[4], 0], [training_set[5], 0]])
+   trainMaybe([[training_set[0], 0], [training_set[1], 0], [training_set[2], 0], [training_set[3], 0], [training_set[4], 1], [training_set[5], 1]])
 
-#trains the yes perceptron
+#trains the yes perceptron to return 1 for 'yes'
+#and 0 for both 'no' and 'maybe'
 def trainYes(training_set):
    global weightsYes
    for k in range(400):
@@ -93,7 +108,8 @@ def trainYes(training_set):
       if errors == 0:
          break
 
-#trains the no perceptron
+#trains the no perceptron to return 1 for 'no'
+#and 0 for both 'yes' and 'maybe'
 def trainNo(training_set):
    global weightsNo
    for k in range(400):
@@ -108,7 +124,8 @@ def trainNo(training_set):
       if errors == 0:
          break
 
-#trains the maybe perceptron
+#trains the maybe perceptron to return 1 for 'maybe'
+#and 0 for both 'yes' and 'no'
 def trainMaybe(training_set):
    global weightsMaybe
    for k in range(400):
@@ -137,22 +154,8 @@ def initWeights():
       weightsMaybe.append(r.random())   
 
 
-
+#Call to initialize the weights
 initWeights()
-#arrayYes = [1.0, 1.403776924836857, 1.5832014471721996, 1.5795352152008399, 1.7825630564406683, 1.747930765862082, 1.5957430879058976, 1.5383704542634984, 1.5877580060817162, 1.6224476028114385, 1.5511625459877452, 1.5682342029972915, 1.5322415558295053, 1.5043744421406737, 1.6430015480546698, 1.7049854628432752, 1.770062804302114, 1.9101468167439477, 1.7861959843622164, 1.7233614620126285]
-arrayYes = [1.0, 1.3532338157620698, 1.5116653349808271, 1.3993980969544908, 1.5723993896152104, 1.5687563353039942, 1.5676748630378194, 1.6059230112792973, 1.6250753356888303, 1.5827371695632746, 1.5164957878980967, 1.5397440142163061, 1.5635007659738493, 1.6153040143077775, 1.6259028726751992, 1.7664829439686527, 1.7974609616429447, 1.7270493032267749, 1.6596417111942952, 1.6182082776561468]
-arrayNo = [1.0, 1.4196126432372365, 1.5612856283427641, 1.4958372768117594, 1.6384810328379353, 1.616265107117357, 1.5951359148022854, 1.668970086392076, 1.7417600939019255, 1.7053395738522497, 1.6856778987297718, 1.705714291735585, 1.7354264974170133, 1.8014927943073413, 1.8157216045498219, 1.8088849025166387, 1.6776419103943221, 1.5789968962321614, 1.5944152620306697, 1.6005728986139767]
-arrayMaybe = [1.0, 1.4375446235350784, 1.632546867782761, 1.5690259252239347, 1.6575212653481173, 1.4945567882781847, 1.5397837909802856, 1.5196210164098789, 1.5061753940747045, 1.6152381352203309, 1.5365281582730554, 1.5426723946849941, 1.5851742218950928, 1.5581901527597386, 1.5091823769069017, 1.578406479697386, 1.5768756399258264, 1.6555194839403042, 1.7321073160591185, 1.7674706904649178]
-
-train([arrayYes, arrayNo, arrayMaybe])
-
-
-
-
-
-
-
-
 
 
 
@@ -161,6 +164,9 @@ train([arrayYes, arrayNo, arrayMaybe])
 def displayHist():
    global f
    global currentMagBin
+   global TRAIN_COUNT
+   global VECTORS_TO_TRAIN
+   TRAIN_COUNT += 1
    #Test wave
    #wavFile = wave.open('C:\Users\Tyler\Desktop\warning.wav')
 
@@ -212,21 +218,6 @@ def displayHist():
       left = array (out)
       right = left
 
-   # transformedData = scipy.fft(left)
-   # totalTransformed = transformedData.size
-   # magnitude = zeros(totalTransformed)
-   # i = 0
-   # for datum in transformedData:
-      # magnitude[i] = sqrt(square(datum.real) + square(datum.imag))
-      # i += 1
-
-   # magnitudeHalved = magnitude[0:totalTransformed/2]
-   # totalSum =  sum(magnitudeHalved)
-   # relMag = zeros(magnitudeHalved.size)
-
-   # Xdb = 20*scipy.log10(scipy.absolute(transformedData))
-   # fn = scipy.linspace(0, 44100, magnitudeHalved.size)
-
 
    xfft = abs(scipy.fft(left))
    where(xfft,0,1e-17)
@@ -268,6 +259,7 @@ def displayHist():
          minFreqBin = magBin[i]
 
    magBin = magBin / minFreqBin
+   magBin = map(lambda x: x/min(magBin), magBin)
 
    a = f.add_subplot(121)
    a.clear()
@@ -293,20 +285,26 @@ def displayHist():
    #ATTN: this is the call to the function you should
    #fill in with AI code, with the variable you want included.
    currentMagBin = magBin
-   parseNewData(magBin)
+
+   if(TRAIN_COUNT <=6):
+      VECTORS_TO_TRAIN.append(magBin)
+      if(TRAIN_COUNT == 6):
+         train(VECTORS_TO_TRAIN)
+   else:
+      parseNewData(magBin)
 
 #ATTN: fill in with AT code :P
 def parseNewData(input_vector):
 	#TODO: word recog
    print input_vector
 
-   input_vector = map(lambda x: x/min(input_vector), input_vector)
+##   input_vector = map(lambda x: x/min(input_vector), input_vector)
 ##   print input_vector
    summed = [sum_sig(input_vector, 'yes'), sum_sig(input_vector, 'no'), sum_sig(input_vector, 'maybe')]
    result = []
    for val in summed:
       result.append(1 if val > 0.5 else 0)
-   print input_vector
+##   print input_vector
    print result
    
    entry.delete(0, len(entry.get()))
@@ -319,9 +317,36 @@ def set_random_weights(weight_vector):
   for index in range(len(weight_vector)):
     weight_vector[index] = r.uniform(-1,1)
   return weight_vector
-  
+
+
+
+
+#Window 1 for training instructions
+learn = Tk.Tk()
+learn.wm_title('Training Instructions')
+learn.geometry('450x600')
+w = Tk.Canvas(learn, width=400, height=400)
+w.pack()
+w.create_text(135,150, text = '\n\
+                               First train the perceptron by:\n   \
+                                  1) Push record and say yes. Repeat a second time\n   \
+                                  2) Push record and say no. Repeat a second time\n   \
+                                  3) Push record and say maybe. Repeat a second time\n\n\
+                                Once you have finished the training, then you can test by:\n   \
+                                  1) Push record and say one of yes, no, or maybe\n\n\
+                                Close this window to continue.', font=("Helvetica", 14))
+closebutton = Tk.Button(learn, text="Close", command = learn.destroy)
+closebutton.grid(row=0, column=0)
+closebutton.pack(padx=8, pady=8)
+
+learn.mainloop()
+
+
+
+
+#Window 2 for testing
 root = Tk.Tk()
-root.wm_title("Embedding in TK")
+root.wm_title("Voice Recognition")
 
 frame = Tk.Frame(root)
 frame.grid(row = 0, column = 0)
@@ -363,8 +388,6 @@ root.mainloop()
 
 # button = Tk.Button(frame, text="QUIT", command="frame.quit")
 # button.pack(side=LEFT)
-
-
 
 
 
